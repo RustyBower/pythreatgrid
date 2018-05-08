01802e672474c54545163c25e9de8540b461c8a8
Rusty Bower <rusty@rustybower.com>
2018-05-08 18:06:46 -0500

    Updating setup.py with supported Python versions

--- setup.py
+++ setup.py
@@ -9,13 +9,23 @@ with codecs.open('HISTORY', 'r', 'utf-8') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 setup(name='pythreatgrid',
-      version='1.0',
+      version='1.0.1',
       description='Python ThreatGrid API wrapper',
       long_description=readme + '\n\n' + history,
       author='Stephen Hosom, Rusty Bower',
       author_email='0xhosom@gmail.com, rusty@rustybower.com',
-      download_url='https://github.com/RustyBower/pythreatgrid/archive/1.0.tar.gz',
+      download_url='https://github.com/RustyBower/pythreatgrid/archive/1.0.1.tar.gz',
       url='https://github.com/RustyBower/pythreatgrid',
       packages=['pythreatgrid'],
       install_requires=['requests'],
+      classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+      ]
       )
