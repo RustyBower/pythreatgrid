1e8bad9e78ef73869f644a2faddcdb64aee844cc
Rusty Bower <rusty@rustybower.com>
2018-05-08 18:11:28 -0500

    Bump version: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ with codecs.open('HISTORY', 'r', 'utf-8') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 setup(name='pythreatgrid',
-      version='1.0.1',
+      version='1.0.2',
       description='Python ThreatGrid API wrapper',
       long_description=readme + '\n\n' + history,
       author='Stephen Hosom, Rusty Bower',
       author_email='0xhosom@gmail.com, rusty@rustybower.com',
-      download_url='https://github.com/RustyBower/pythreatgrid/archive/1.0.1.tar.gz',
+      download_url='https://github.com/RustyBower/pythreatgrid/archive/1.0.2.tar.gz',
       url='https://github.com/RustyBower/pythreatgrid',
       packages=['pythreatgrid'],
       install_requires=['requests'],
