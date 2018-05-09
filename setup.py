51c7eca159363bf3d51de2ff9d28cb7115245bf4
Rusty Bower <rusty@rustybower.com>
2018-05-09 01:20:36 -0500

    Fixing typo

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
+import os
 from distutils.core import setup
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(name='pythreatgrid',
       version='1.0.6',
       description='Python ThreatGrid API wrapper',
-      long_description=read('README.md') + '\n\n' + read('HISTORY')
+      long_description=read('README.md') + '\n\n' + read('HISTORY'),
       author='Stephen Hosom, Rusty Bower',
       author_email='0xhosom@gmail.com, rusty@rustybower.com',
       download_url='https://github.com/RustyBower/pythreatgrid/archive/v1.0.6.tar.gz',
