e9be43cc7f96eaf52befb01675efa929bcf71bee
Rusty Bower <rusty@rustybower.com>
2018-05-09 01:10:15 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
-import codecs
 from distutils.core import setup
 
-with codecs.open('README.md', 'r', 'utf-8') as readme_file:
-    readme = readme_file.read()
-
-with codecs.open('HISTORY', 'r', 'utf-8') as history_file:
-    history = history_file.read().replace('.. :changelog:', '')
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='pythreatgrid',
       version='1.0.4',
       description='Python ThreatGrid API wrapper',
-      long_description=readme + '\n\n' + history,
+      long_description=read('README.md') + '\n\n' + read('HISTORY')
       author='Stephen Hosom, Rusty Bower',
       author_email='0xhosom@gmail.com, rusty@rustybower.com',
       download_url='https://github.com/RustyBower/pythreatgrid/archive/v1.0.4.tar.gz',
